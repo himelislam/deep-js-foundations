d5a8fa2a863e4c08f1e3f3fe8533df5a922f1675
Himel Islam <himel7100@gmail.com>
2024-03-21 02:42:03 +0600

    key press project done-2

--- 02_9_fundamental_objects.js
+++ 02_9_fundamental_objects.js
@@ -49,22 +49,28 @@ if (!Object.is || true) {
 
 
 // tests:
-console.log(Object.is(42, 42) === true);
-console.log(Object.is("foo", "foo") === true);
-console.log(Object.is(false, false) === true);
-console.log(Object.is(null, null) === true);
-console.log(Object.is(undefined, undefined) === true);
-console.log(Object.is(NaN, NaN) === true);
-console.log(Object.is(-0, -0) === true);
-console.log(Object.is(0, 0) === true);
+// console.log(Object.is(42, 42) === true);
+// console.log(Object.is("foo", "foo") === true);
+// console.log(Object.is(false, false) === true);
+// console.log(Object.is(null, null) === true);
+// console.log(Object.is(undefined, undefined) === true);
+// console.log(Object.is(NaN, NaN) === true);
+// console.log(Object.is(-0, -0) === true);
+// console.log(Object.is(0, 0) === true);
 
-console.log(Object.is(-0, 0) === false);
-console.log(Object.is(0, -0) === false);
-console.log(Object.is(0, NaN) === false);
-console.log(Object.is(NaN, 0) === false);
-console.log(Object.is(42, "42") === false);
-console.log(Object.is("42", 42) === false);
-console.log(Object.is("foo", "bar") === false);
-console.log(Object.is(false, true) === false);
-console.log(Object.is(null, undefined) === false);
-console.log(Object.is(undefined, null) === false);
+// console.log(Object.is(-0, 0) === false);
+// console.log(Object.is(0, -0) === false);
+// console.log(Object.is(0, NaN) === false);
+// console.log(Object.is(NaN, 0) === false);
+// console.log(Object.is(42, "42") === false);
+// console.log(Object.is("42", 42) === false);
+// console.log(Object.is("foo", "bar") === false);
+// console.log(Object.is(false, true) === false);
+// console.log(Object.is(null, undefined) === false);
+// console.log(Object.is(undefined, null) === false);
+
+
+const isTrue = true;
+
+isTrue.name = "Himel";
+console.log(isTrue.name);
