40930fbbaeea16945aefbfe9e8636fa3ef391d7a
Himel Islam <himel7100@gmail.com>
2024-08-15 01:01:14 +0600

    function

--- Objects/4-ways-of-invoke-function.js
+++ Objects/4-ways-of-invoke-function.js
@@ -62,4 +62,28 @@ askAgain2.apply(workshop2, ["Explicit Way"])
 // explicitly saying which context it should use in both the call and apply method.
 // the one difference between call and apply is the second argument which call takes individually and apply takes as an Array.
 
+// Variation of Explicit
+
+// Hard Binding
+
+var workshop3 = {
+    teacher: "Himel Islam",
+    askAgain3(question){
+        console.log(this.teacher, question);
+    }
+}
+
+setTimeout(workshop3.ask, 10, "helllo"); // it will open the context in global since its an HTML setTimeOut cb function
+setTimeout(workshop3.ask.bind(workshop3), 10, "Hard Bound This"); // therefore we need to use the hard binding the include the context
+
 //****************************************************************** */
+
+// 3. new binding -> using new Keyword.
+// Its basically invoking a function with a empty object concept. It invoke the function with a context of a empty object context.
+
+function ask2(question){
+    console.log(this.teacher, question);
+}
+
+const newEmptyObject = new ask2("Invoking this function with this keyword with a empty object context");
+// undifined Invoking this function with this keyword with a empty object context  ---> res
